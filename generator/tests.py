aece561497b258aff92ef8a1fadb05124df8a77c
Arthur Caillau <caillau@kth.se>
2013-05-24 18:50:53 +0200

    New app: Generator!

---
+++ generator/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from models import Generator
+from datetime import datetime
+
+
+class ModelTest(TestCase):
+
+    def test(self):
+        self.fail('TODO: write unit tests')
