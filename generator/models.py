89036d55d149f0954bb69a7ab0b9269cb73f6517
Arthur Caillau <caillau@kth.se>
2013-05-25 14:48:18 +0200

    Generator passes unit tests

--- generator/models.py
+++ generator/models.py
@@ -1,5 +1,50 @@
-from django.db import models
+import random
+
 
 # Create your models here.
-class Generator(models.Model):
-    pass
+class Generator():
+
+    last_password_generated = None
+
+    def __init__(self, upper_letters=True, lower_letters=True,
+                 numbers=True, special_characters=True, length=10):
+        self.upper_letters = upper_letters
+        self.lower_letters = lower_letters
+        self.numbers = numbers
+        self.special_characters = special_characters
+        self.length = length
+
+    def generate(self):
+
+        allowed_char = []
+        password = []
+
+        if self.lower_letters:
+            allowed_char += map(chr, range(97, 123))
+        if self.upper_letters:
+            allowed_char += map(chr, range(65, 91))
+        if self.numbers:
+            allowed_char += [str(i) for i in xrange(0, 10)]
+        if self.special_characters:
+            allowed_char += map(chr, range(33, 47))
+        random.shuffle(allowed_char)
+
+        n = len(allowed_char)
+        if n == 0:
+            raise GeneratorException
+        else:
+            for _ in xrange(self.length):
+                index = random.randint(0, n-1)
+                password.append(allowed_char[index])
+
+            self.last_password_generated = ''.join(password)
+            return self.last_password_generated
+
+
+class GeneratorException(Exception):
+
+    def __init__(self):
+        self.error = 'Can not generate a Key'
+
+    def __str__(self):
+        return self.error
--- generator/tests.py
+++ generator/tests.py
@@ -1,9 +1,34 @@
 from django.test import TestCase
-from models import Generator
-from datetime import datetime
+from models import Generator, GeneratorException
 
 
 class ModelTest(TestCase):
 
-    def test(self):
-        self.fail('TODO: write unit tests')
+    def setUp(self):
+        self.generator = Generator(upper_letters=True,
+                                   lower_letters=True,
+                                   numbers=True,
+                                   special_characters=True,
+                                   length=25)
+
+    def test_key_generation(self):
+
+        password = self.generator.generate()
+        self.assertEquals(len(password), 25)
+        self.assertEquals(self.generator.last_password_generated, password)
+
+        self.generator.length = 0
+        self.assertEquals(self.generator.generate(), '')
+
+        self.generator.upper_letters = False
+        self.generator.lower_letters = False
+        self.generator.numbers = False
+        self.generator.special_characters = False
+
+        try:
+            password = self.generator.generate()
+        except GeneratorException, e:
+            pass
+            # print e
+        else:
+            self.fail('Exception not thrown')
