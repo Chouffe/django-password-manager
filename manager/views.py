c26ed938bede666c226be4bd366ad38cbc5cd7db
Arthur Caillau <caillau@kth.se>
2013-05-26 17:50:42 +0200

    Cleaned views

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,5 @@
 from manager.forms import EntryForm
 from django.shortcuts import render
-from generator.models import Generator
 from manager.models import CryptoEngine, Entry
 from django.views.generic import DetailView
 from django.views.generic import CreateView
@@ -11,39 +10,24 @@ from django.views.generic import DeleteView
 engine = CryptoEngine(master_key='testofanewawesomekey')
 
 
-def entry(request):
-
-    if request.method == 'POST':
-        form = EntryForm(request.POST)
-        if form.is_valid():
-            message = 'Success'
-            send = True
-            entry = form.save(commit=False)
-            entry.password = engine.encrypt(Generator(length=25).generate())
-            entry.save()
-    else:
-        form = EntryForm()
-
-    return render(request, 'entry.html', locals())
-
-
 class EntryDetailView(DetailView):
+    """ Details about a given entry """
+
     model = Entry
     context_object_name = 'entry'
     template_name = 'manager/entry_get.html'
 
     def get_context_data(self, **kwargs):
         context = super(EntryDetailView, self).get_context_data(**kwargs)
-        # print context
-        # print context['entry'].__dict__
         context['decrypted_password'] = engine.decrypt(
             context['entry'].password)
         return context
 
 
 class EntryCreate(CreateView):
+    """ Enables creation of new entries """
+
     model = Entry
-    # fields = ['title', 'url', 'username', 'comment', 'expires']
     template_name = 'manager/entry_create.html'
     form_class = EntryForm
     # success_url = todo
@@ -52,11 +36,7 @@ class EntryCreate(CreateView):
 
         form = EntryForm(request.POST)
         if form.is_valid():
-            message = 'Hello World!!!!'
-            # send = True
             entry = form.save(commit=False)
-            # entry.password = engine.encrypt(Generator(length=25).generate())
-            print form.cleaned_data
             entry.password = engine.encrypt(form.cleaned_data['password'])
             entry.save()
             # return render(request, self.success_url, locals())
@@ -66,9 +46,9 @@ class EntryCreate(CreateView):
 
 
 class EntryUpdate(UpdateView):
+    """ Enables update of a given entry """
 
     model = Entry
-    # fields = ['title', 'url', 'username', 'comment', 'expires']
     template_name = 'manager/entry_update.html'
     form_class = EntryForm
     # FIXME
@@ -76,6 +56,8 @@ class EntryUpdate(UpdateView):
 
 
 class EntryDelete(DeleteView):
+    """ Enables deletion of a given entry """
+
     model = Entry
     context_object_name = 'entry'
     template_name = 'manager/entry_delete.html'
