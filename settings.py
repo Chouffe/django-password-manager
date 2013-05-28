0b6aeeeb2d5e93642267d896aa58e850003e1848
Arthur Caillau <caillau@kth.se>
2013-05-28 16:36:30 +0200

    Added a new app api

--- settings.py
+++ settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'manager',
     'generator',
+    'api',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
