beb764a07b4183d91783e23eb9776d67dd7f8628
Arthur Caillau <caillau@kth.se>
2013-05-30 16:31:24 +0200

    Javascript added to play with the dom and the API

--- static/bootstrap/js/manager.js
+++ static/bootstrap/js/manager.js
@@ -12,6 +12,61 @@ $(function() {
     });
 });
 
+
+
+function cleanEntries() {
+    $('#table-entries').remove();
+}
+
+function fillEntries(entries) {
+
+    cleanEntries();
+    if( entries.length == 0) {
+        console.log('No entries');
+    }
+    else {
+
+        // create table
+        var $table = $('<table id="table-entries" class="table table-striped">');
+
+        // caption
+        // $table.append('<caption>Entry Table</caption>');
+
+        // thead
+        $table
+        .append('<thead>').children('thead')
+        .append('<tr />').children('tr').append('<th>#</th><th>Title</th><th>Url</th><th>Username</th><th>Expires</th><th id="show-passwords">Password <i class="pull-right icon-eye-open"></i></th>');
+
+        //tbody
+        var $tbody = $table.append('<tbody />').children('tbody');
+
+        // add row
+        for ( var i in entries ) {
+
+            $tbody.append('<tr />').children('tr:last')
+            .append("<td><a class='' href=''><i class='icon-cog'></i></a></td>")
+            .append("<td>" + entries[i]['title'] + "</td>")
+            .append("<td>" + entries[i]['url'] + "</td>")
+            .append("<td>" + entries[i]['username'] + "</td>")
+            .append("<td>" + entries[i]['expires'] + "</td>")
+            .append('<td>' + entries[i]['password'] + "<a class='pull-right' href=''><i class='icon-remove'></i></a></td>");
+
+        }
+
+
+        // add table to dom
+        $table.appendTo('#table-container');
+    }
+
+}
+
+function searchEntries(cat) {
+
+    $.getJSON('/api/search.json', { category: cat }).done(function(data) {
+        fillEntries(data);
+    });
+}
+
 function loadTypeAhead() {
 
     $.get('/api/entries.json', function(data) {
