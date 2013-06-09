e0e448eef987654a5ef0dfdec3dea5f356c036c7
Arthur Caillau <caillau@kth.se>
2013-06-09 17:13:47 +0200

    Brute Force protection

--- settings.py
+++ settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'manager',
     'generator',
     'api',
+    'BruteBuster',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
